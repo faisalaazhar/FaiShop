a40bc00e7954aae6df4fbc65ea63f774d920ad4f
Faisal <faisalazhar.itu@gmail.com>
2022-02-26 23:10:27 +0600

    Rating of product available now

--- frontend1/src/components/Product.js
+++ frontend1/src/components/Product.js
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import Rating from './Rating'
 
 const Product = ({product}) => {
   return (
@@ -15,7 +16,10 @@ const Product = ({product}) => {
 
             <Card.Text as='div'>
               <div className="my-3 ">
-                {product.rating} from {product.numReviews} reviews
+                <Rating 
+                  value={product.rating} 
+                  text={`${product.numReviews} reviews`} 
+                />
               </div>
             </Card.Text>
 
