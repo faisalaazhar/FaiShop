334d1cad835b5cf56c4e677493ee8f7f30a6f739
Faisal <faisalazhar.itu@gmail.com>
2022-02-26 13:03:17 +0600

    Product list functionality added in the home page

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Container } from 'react-bootstrap'
-import Header from "./componets/Header"
-import Footer from "./componets/Footer"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Fai Shop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
