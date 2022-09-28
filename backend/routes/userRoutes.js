17d527dc29e08a1a1a53d2f57726eee65b2134ca
Faisal <faisalazhar.itu@gmail.com>
2022-09-28 22:03:50 +0600

    added payment end ponit in server and reducers & actions at frontend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,10 @@
 import express from 'express'
-import {authUser,registerUser, getUserProfile, updateUserProfile} from '../controllers/userController.js'
+import {
+    authUser,
+    registerUser, 
+    getUserProfile, 
+    updateUserProfile
+} from '../controllers/userController.js'
 import {protect} from '../middleware/authMiddleware.js'
 
 const router = express.Router()
