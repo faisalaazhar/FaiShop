9742d024a6cb03bb95872e11977fcffc724b77a0
Faisal <faisalazhar.itu@gmail.com>
2022-09-28 15:59:05 +0600

    created order screen

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from "./screens/ProfileScreen"
 import ShippingScreen from "./screens/ShippingScreen"
 import PaymentScreen from "./screens/PaymentScreen"
 import PlaceOrderScreen from "./screens/PlaceOrderScreen"
+import OrderScreen from "./screens/OrderScreen"
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
+            <Route path="/order/:id" element={<OrderScreen />} exact />
             <Route path="/placeorder" element={<PlaceOrderScreen />} exact />
             <Route path="/payment" element={<PaymentScreen />} exact />
             <Route path="/shipping" element={<ShippingScreen />} exact />
